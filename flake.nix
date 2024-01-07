0990867f79b5049b675fdf920d79271f7f61663b
Bikku <bikku+dev@slmail.me>
2024-01-07 16:32:29 +0100

    Update from 23.05 to 23.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
 	description = "A very basic flake";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 		home-manager = {
-			url = "github:nix-community/home-manager/release-23.05";
+			url = "github:nix-community/home-manager/release-23.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
@@ -32,7 +32,7 @@
 					home = {
 						username = "bikku";
 						homeDirectory = "/home/bikku";
-						stateVersion = "23.05";
+						stateVersion = "23.11";
 					};
 				}
 				];
