d34e33d8f6bcb0037b8223e1ff02a1dc700d7a31
Bikku <bikku+dev@slmail.me>
2024-01-07 16:11:30 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+	description = "A very basic flake";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+		home-manager = {
+			url = "github:nix-community/home-manager/release-23.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, home-manager }:
+		let
+		system = "x86_64-linux";
+	pkgs = import nixpkgs {
+		inherit system;
+		config.allowUnfree = true;
+	};
+	lib = nixpkgs.lib;
+	in {
+		nixosConfigurations = {
+			bikku = lib.nixosSystem {
+				inherit system;
+				modules = [ ./configuration.nix ];
+			};
+		};
+		homeConfigurations = {
+			bikku = home-manager.lib.homeManagerConfiguration {
+				pkgs = pkgs;
+				modules = [
+				{
+					home = {
+						username = "bikku";
+						homeDirectory = "/home/bikku";
+						stateVersion = "23.05";
+					};
+				}
+				];
+#          inherit system.pkgs;
+#          username = "bikku";
+#          homeDirectory = "/home/bikku";
+#          configuration = {
+#           imports = [    ]
+#           };
+			};
+		};
+	};
+}
