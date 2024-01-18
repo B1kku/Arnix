8626073aaef1981d21ce4b40f11c0615362a73da
Bikku <bikku+dev@slmail.me>
2024-01-18 17:28:10 +0100

    Move gnome to a hm module, remove old firefox stuff.

---
+++ dotfiles/gnome.nix
@@ -0,0 +1,32 @@
+{ lib, ...}:
+
+{
+  # config.services.xserver.desktopManager.gnome.enable = true;
+  # dconf watch / & dconf dump > ... for debugging
+  dconf.settings = with lib.hm.gvariant; {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = true;
+    };
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+      dynamic-workspaces = false;
+      workspaces-only-on-primary = false;
+    };
+    "org/gnome/desktop/wm/preferences" = { num-workspaces = 4; };
+    "org/gnome/shell/app-switcher" = { current-workspace-only = false; };
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
+      night-light-temperature = mkUint32 3700;
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      mic-mute = [ "<Alt>z" ];
+      volume-up = [ "<Alt>KP_Up" ];
+      volume-down = [ "<Alt>KP_Down" ];
+      previous = [ "<Alt>KP_Left" ];
+      play = [ "<Alt>KP_Begin" ];
+      next = [ "<Alt>KP_Right" ];
+    };
+  };
+
+}
--- users/bikku/home.nix
+++ users/bikku/home.nix
@@ -4,7 +4,11 @@
   programs.home-manager.enable = true;
   home.username = "bikku";
   home.homeDirectory = "/home/bikku";
-  imports = [ ../../dotfiles/firefox.nix ../../dotfiles/neovim.nix ];
+  imports = [ 
+    ../../dotfiles/firefox.nix 
+    ../../dotfiles/neovim.nix 
+    ../../dotfiles/gnome.nix
+  ];
   programs.bash.enable = true;
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
@@ -19,33 +23,6 @@
     fira-code-nerdfont
     prismlauncher
   ];
-  # config.services.xserver.desktopManager.gnome.enable = true;
-  # dconf watch / & dconf dump > ... for debugging
-  dconf.settings = with lib.hm.gvariant; {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      enable-hot-corners = true;
-    };
-    "org/gnome/mutter" = {
-      edge-tiling = true;
-      dynamic-workspaces = false;
-      workspaces-only-on-primary = false;
-    };
-    "org/gnome/desktop/wm/preferences" = { num-workspaces = 4; };
-    "org/gnome/shell/app-switcher" = { current-workspace-only = false; };
-    "org/gnome/settings-daemon/plugins/color" = {
-      night-light-enabled = true;
-      night-light-temperature = mkUint32 3700;
-    };
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      mic-mute = [ "<Alt>z" ];
-      volume-up = [ "<Alt>KP_Up" ];
-      volume-down = [ "<Alt>KP_Down" ];
-      previous = [ "<Alt>KP_Left" ];
-      play = [ "<Alt>KP_Begin" ];
-      next = [ "<Alt>KP_Right" ];
-    };
-  };
   # Don't change randomly, used for internals.
   home.stateVersion = "23.11";
 }
