c9fa727e5db8275ef825aa7669ba67ef73130ae0
Bikku <bikku+dev@slmail.me>
2024-01-24 11:02:51 +0100

    Woops

--- dotfiles/discord.nix
+++ dotfiles/discord.nix
@@ -7,12 +7,9 @@
   # Perhaps it'd be better to just add a desktopitems module instead?
   xdg.desktopEntries.vesktop = {
       name = "Discord";
-      desktopName = "Discord";
       exec = "vesktop %U";
       icon = "discord";
-      startupWMClass = "Discord";
       genericName = "Internet Messenger";
-      keywords = [ "discord" "vencord" "electron" "chat" ];
       categories = [ "Network" "InstantMessaging" "Chat" ];
-  }
+  };
 }
