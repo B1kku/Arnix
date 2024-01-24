52f121b48cef48c85a5454b7fabd5ec247dce7e7
Bikku <bikku+dev@slmail.me>
2024-01-24 10:10:00 +0100

    Fix neovim desktop item, why'd I want it to be called neovim wrapper?

--- dotfiles/neovim.nix
+++ dotfiles/neovim.nix
@@ -3,7 +3,6 @@
     source = ./neovim;
     target = ".config/nvim/";
   };
-
   programs.neovim = {
     enable = true;
     defaultEditor = true;
@@ -21,7 +20,33 @@
       ripgrep # Telescope softdep
       fd # Telescope dependency?
       clang # Telescope softdep
+      # vector # Treesitter dep? I've seen a better way to do this somewhere...
     ] ++ language-server-providers;
   };
-
+  xdg.desktopEntries.nvim = {
+    name = "Neovim";
+    genericName = "Text Editor";
+    type = "Application";
+    exec = "nvim %u";
+    terminal = true;
+    categories = [ "Utility" "TextEditor" ];
+    icon = "nvim";
+    mimeType = [
+      "text/english"
+      "text/plain"
+      "text/x-makefile"
+      "text/x-c++hdr"
+      "text/x-c++src"
+      "text/x-chdr"
+      "text/x-csrc"
+      "text/x-java"
+      "text/x-moc"
+      "text/x-pascal"
+      "text/x-tcl"
+      "text/x-tex"
+      "application/x-shellscript"
+      "text/x-c"
+      "text/x-c++"
+    ];
+  };
 }
