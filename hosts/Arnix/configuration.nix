8fc054f4e5598ea00d5e501bad64e7235b39c631
Bikku <bikku+dev@slmail.me>
2024-02-04 21:26:49 +0100

    Setup controller properly... Ish

--- hosts/Arnix/configuration.nix
+++ hosts/Arnix/configuration.nix
@@ -8,6 +8,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../../modules/powera-controller.nix
     home-manager.nixosModules.home-manager
   ];
   nix = {
---
+++ modules/powera-controller.nix
@@ -0,0 +1,18 @@
+{...}:
+{
+  services.udev.extraHwdb = ''
+    evdev:name:BDA NSW wired controller:*
+      KEYBOARD_KEY_90002=btn_c      # A 
+      KEYBOARD_KEY_90003=btn_east   # B
+      KEYBOARD_KEY_90001=btn_west   # X
+      KEYBOARD_KEY_90004=btn_south  # Y
+      KEYBOARD_KEY_90005=btn_z      # L
+      KEYBOARD_KEY_90006=btn_tl     # R
+      KEYBOARD_KEY_90007=btn_tr     # LT
+      KEYBOARD_KEY_90008=btn_tl2    # RT
+      KEYBOARD_KEY_9000b=btn_start  # L
+      KEYBOARD_KEY_9000c=btn_mode   # R
+      KEYBOARD_KEY_9000e=btn_tr2    # Select
+      KEYBOARD_KEY_9000d=btn_select # Start
+  '';
+}
