8c7beeab54a095ddafc150775c07d78d98f598ca
Bikku <bikku+dev@slmail.me>
2024-02-09 20:13:50 +0100

    Add lutris runners declaratively

--- dotfiles/games.nix
+++ dotfiles/games.nix
@@ -1,133 +1,38 @@
-{ inputs, pkgs, ... }:
+{ inputs, pkgs, lib, ... }:
 let
   custom-lutris = (pkgs.lutris.override {
-    extraPkgs = pkgs: with pkgs; [wineWowPackages.stableFull];
-    extraLibraries = pkgs:
+    extraPkgs = pkgs:
       with pkgs; [
+        wineWowPackages.stableFull
+        gamemode
+        mangohud
         winetricks
-        # mesa.llvmPackages.llvm.lib
-        # vulkan-loader
-        # expat
-        # wayland
-        # xorg.libxcb
-        # xorg.libXdamage
-        # xorg.libxshmfence
-        # xorg.libXxf86vm
-        # libelf
-        # (lib.getLib elfutils)
-        # # Without these it silently fails
-        # xorg.libXinerama
-        # xorg.libXcursor
-        # xorg.libXrender
-        # xorg.libXScrnSaver
-        # xorg.libXi
-        # xorg.libSM
-        # xorg.libICE
-        # gnome2.GConf
-        # curlWithGnuTls
-        # nspr
-        # nss
-        # cups
-        # libcap
-        # SDL2
-        # libusb1
-        # dbus-glib
-        # gsettings-desktop-schemas
-        # ffmpeg
-        # libudev0-shim
-        #
-        # # Verified games requirements
-        # fontconfig
-        # freetype
-        # xorg.libXt
-        # xorg.libXmu
-        # libogg
-        # libvorbis
-        # SDL
-        # SDL2_image
-        # glew110
-        # libdrm
-        # libidn
-        # tbb
-        # zlib
-        #
-        # # SteamVR
-        # udev
-        # dbus
-        #
-        # # Other things from runtime
-        # glib
-        # gtk2
-        # bzip2
-        # flac
-        # freeglut
-        # libjpeg
-        # libpng
-        # libpng12
-        # libsamplerate
-        # libmikmod
-        # libtheora
-        # libtiff
-        # pixman
-        # speex
-        # SDL_image
-        # SDL_ttf
-        # SDL_mixer
-        # SDL2_ttf
-        # SDL2_mixer
-        # libappindicator-gtk2
-        # libdbusmenu-gtk2
-        # libindicator-gtk2
-        # libcaca
-        # libcanberra
-        # libgcrypt
-        # libunwind
-        # libvpx
-        # librsvg
-        # xorg.libXft
-        # libvdpau
-        # # Not formally in runtime but needed by some games
-        # at-spi2-atk
-        # at-spi2-core # CrossCode
-        # gst_all_1.gstreamer
-        # gst_all_1.gst-plugins-ugly
-        # gst_all_1.gst-plugins-base
-        # json-glib # paradox launcher (Stellaris)
-        # libdrm
-        # libxkbcommon # paradox launcher
-        # libvorbis # Dead Cells
-        # libxcrypt # Alien Isolation, XCOM 2, Company of Heroes 2
-        # mono
-        # ncurses # Crusader Kings III
-        # openssl
-        # xorg.xkeyboardconfig
-        # xorg.libpciaccess
-        # xorg.libXScrnSaver # Dead Cells
-        # icu # dotnet runtime, e.g. Stardew Valley
-        #
-        # # screeps dependencies
-        # gtk3
-        # zlib
-        # atk
-        # cairo
-        # freetype
-        # gdk-pixbuf
-        # fontconfig
-        #
-        # # Prison Architect
-        # libGLU
-        # libuuid
-        # libbsd
-        # alsa-lib
-        #
-        # # Loop Hero
-        # # FIXME: Also requires openssl_1_1, which is EOL. Either find an alternative solution, or remove these dependencies (if not needed by other games)
-        # libidn2
-        # libpsl
-        # nghttp2.lib
-        # rtmpdump
       ];
   });
+  # Add any lutris extra runners here, can also configure with runner.config
+  lutris-runners = with pkgs; {
+    cemu.package = cemu;
+    yuzu.package = yuzu-early-access;
+  };
 in {
-  home.packages = with pkgs; [ custom-lutris winetricks wineWowPackages.stableFull steam-run ];
+  xdg.configFile = lib.attrsets.mapAttrs' (name: value: {
+    name = "lutris-${name}";
+    value = let 
+      config = (lib.recursiveUpdate {
+        system = { locale = ""; disable_runtime = true; };
+        ${name} = { runner_executable = "${value.package}/bin/${name}"; };
+      } (lib.optionalAttrs (value ? "config") value.config));
+    in {
+      target = "lutris/runners/${name}.yml";
+      text = builtins.toJSON config;
+    };
+  }) lutris-runners;
+
+  home.packages = with pkgs; [
+    custom-lutris
+    winetricks
+    wineWowPackages.stableFull
+    steam-run
+    cemu
+  ];
 }
