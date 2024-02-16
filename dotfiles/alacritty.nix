e2bae8f522a9c0d8b55c323c50f0c819ac54c3f4
Bikku <bikku+dev@slmail.me>
2024-02-16 20:25:45 +0100

    Fix alacritty complaining nonstop...

--- dotfiles/alacritty.nix
+++ dotfiles/alacritty.nix
@@ -1,26 +1,34 @@
 { lib, pkgs, pkgs-unstable, ... }:
-let terminal-font = "FiraCode Nerd Font";
+let
+  TOMLgenerator = pkgs.formats.toml { };
+  terminal-font = "FiraCode Nerd Font";
+  config = {
+    window = {
+      decorations = "None";
+      opacity = 0.75;
+      startup_mode = "Maximized";
+    };
+    font = {
+      normal.family = terminal-font;
+      size = 12;
+    };
+  };
 in {
-
   home.packages = [
     (pkgs.writeShellScriptBin "xdg-terminal-exec" ''
       exec "${lib.getExe pkgs-unstable.alacritty}" -e "$@"
     '')
   ];
+  # This is needed because home manager, although it checks
+  # pkg version, it's not doing it properly, which leads
+  # to a yaml file and therefore alacritty complains nonstop.
+  xdg.configFile = {
+    "alacritty/alacritty.toml".source =
+      TOMLgenerator.generate "alacritty.toml" config;
+  };
   programs.alacritty = {
     enable = true;
     package = pkgs-unstable.alacritty;
-    settings = {
-      # shell.program = "${pkgs.zsh}/bin/zsh";
-      window = {
-        decorations = "None";
-        opacity = 0.75;
-        startup_mode = "Maximized";
-      };
-      font = {
-        normal.family = terminal-font;
-        size = 12;
-      };
-    };
+    # settings = { };
   };
 }
