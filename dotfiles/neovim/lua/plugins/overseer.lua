e2b157db872cd74c3d96f4706dac0802a8ad7314
Bikku <bikku+dev@slmail.me>
2024-02-23 13:53:01 +0100

    Add overseer bindings

--- dotfiles/neovim/lua/plugins/overseer.lua
+++ dotfiles/neovim/lua/plugins/overseer.lua
@@ -1,19 +1,38 @@
 return {
   'stevearc/overseer.nvim',
+  keys = {
+    { "<leader><cr>", [[<cmd>OverseerToggle<CR>]],        "n", silent = true, desc = "Toggle Overseer Tasks" },
+    { "<C-CR>",       [[<cmd>lua OverseerRestart()<CR>]], "n", silent = true, desc = "Restart last task, or choose a template" }
+  },
   config = function()
-    vim.o.exrc = true
+    local overseer = require('overseer')
+    function OverseerRestart()
+      local tasks = overseer.list_tasks({ recent_first = true })
+      if vim.tbl_isempty(tasks) then
+        vim.cmd("OverseerRun")
+      else
+        if (tasks[1].status == "PENDING") then
+          overseer.run_action(tasks[1], "start")
+        elseif (tasks[1].status ~= "RUNNING") then
+          overseer.run_action(tasks[1], "restart")
+        end
+      end
+    end
 
+    vim.o.exrc = true
     local win_opts = {
       winblend = 0,
     }
-    local overseer = require('overseer')
     overseer.setup({
       bundles = {
         autostart_on_load = false
       },
       templates = { "builtin", "java.maven", "java.test", "wrapper" },
       task_list = {
-        direction = "bottom"
+        direction = "bottom",
+        bindings = {
+          ["a"] = "<CMD>OverseerRun<CR>"
+        }
       },
       form = {
         win_opts = win_opts
