4835af954ae0a6ec546c70480477de1eddcdcd7c
Bikku <bikku+dev@slmail.me>
2024-04-09 12:12:00 +0200

    Neovim: fix neovim-remote-wrapper not escaping file paths

--- dotfiles/neovim/nvim-remote-wrapper.sh
+++ dotfiles/neovim/nvim-remote-wrapper.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-command="n ${*}"
 
-exec nvim --server $NVIM --remote-send "<CMD>q<CR><CMD>lua vim.cmd([[$command]])<CR>"
+escaped_files=""
+for file in "$@"
+do
+    escaped_file=$(printf "%q" "$file")
+    escaped_files="$escaped_files $escaped_file"
+done
+exec nvim --server $NVIM --remote-send "<CMD>q<CR><CMD>lua vim.cmd([[n $escaped_files]])<CR>"
+
