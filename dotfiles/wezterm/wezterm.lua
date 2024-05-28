5ef5d984f7843b74bdc18f6a4bceb4f1fb3db98d
Bikku <bikku+dev@slmail.me>
2024-05-28 10:33:09 +0200

    Fix wezterm not working properly when launched with args

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -18,9 +18,13 @@ config = {
   }
 }
 
-wezterm.on('gui-startup', function()
-  local tab, pane, window = mux.spawn_window({})
-  window:gui_window():maximize()
+wezterm.on('gui-attached', function(domain)
+  -- maximize all displayed windows on startup
+  local workspace = mux.get_active_workspace()
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():maximize()
+    end
+  end
 end)
-
 return config
