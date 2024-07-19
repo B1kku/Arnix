74db51d011e41f04d522bd5cb0126399359fe3ee
Bikku <bikku+dev@slmail.me>
2024-07-19 20:16:48 +0200

    Neovim: Switch kotlin_lsp cache directory again
    Kotlin won't create the directory if it doesn't exist, and I can't be
    bothered to check if it exists.

--- dotfiles/neovim/lua/lsp/lsp-config/kotlin_language_server.lua
+++ dotfiles/neovim/lua/lsp/lsp-config/kotlin_language_server.lua
@@ -2,7 +2,7 @@ local M = {}
 local fn = vim.fn
 M.lsp_setup = {
   init_options = {
-    storagePath = fn.resolve(fn.stdpath("cache") .. "/kotlin_language_server")
+    storagePath = fn.resolve(fn.stdpath("cache"))
   }
 }
 
