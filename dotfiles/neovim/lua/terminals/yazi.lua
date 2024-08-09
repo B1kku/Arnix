e66e2d4a4b7ff12b9c395d2ee2c5b6e3a3ba81c6
Bikku <bikku+dev@slmail.me>
2024-08-10 00:07:38 +0200

    Neovim: Yazi, fix choosing current bufpath properly

--- dotfiles/neovim/lua/terminals/yazi.lua
+++ dotfiles/neovim/lua/terminals/yazi.lua
@@ -1,25 +1,24 @@
 local vfn = vim.fn
 
+local function get_last_buffer()
+  local last_buffer = vim.iter(vim.fn.getbufinfo({ bufloaded = 1, buflisted = 1 })):fold({ lastused = 0 },
+    function(prev_buffer, buffer)
+      if prev_buffer.lastused > buffer.lastused then
+        return prev_buffer
+      else
+        return buffer
+      end
+    end)
+  return (last_buffer.bufnr) and last_buffer or nil
+end
+
 local function get_bufpath(bufnr)
   return vfn.fnamemodify(vfn.bufname(bufnr), ':p')
 end
-local get_yazi_cmd = function()
-  local function check_buffer(bufnr)
-    local buf_id = vfn.bufnr(bufnr)
-    if buf_id == -1 or not vfn.bufloaded(buf_id) then
-      return false
-    end
-    return true
-  end
 
-  local buf_path
-  if check_buffer("#") then
-    buf_path = get_bufpath("#")
-  elseif check_buffer("%") then
-    buf_path = get_bufpath("%")
-  else
-    buf_path = vfn.getcwd()
-  end
+local get_yazi_cmd = function()
+  local last_buffer = get_last_buffer()
+  local buf_path = last_buffer and get_bufpath(last_buffer.bufnr) or vfn.getcwd()
   return "yazi " .. "\"" .. buf_path .. "\""
 end
 
