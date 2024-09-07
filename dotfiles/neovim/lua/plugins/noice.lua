a0de9c98194a49d1debfbd69f1ccad587c47a673
Bikku <bikku+dev@slmail.me>
2024-09-07 13:51:33 +0200

    Neovim: Noice config

--- dotfiles/neovim/lua/plugins/noice.lua
+++ dotfiles/neovim/lua/plugins/noice.lua
@@ -4,20 +4,43 @@ return {
     "folke/noice.nvim",
     dependencies = {
       { "MunifTanjim/nui.nvim" },
-      { "rcarriga/nvim-notify" }
+      { "rcarriga/nvim-notify" },
+      { "hrsh7th/nvim-cmp" }
     },
     config = function()
-      require("notify").setup {
-        background_colour = "#000000"
-      }
       require("noice").setup({
         presets = {
-          lsp_doc_border = true
+          lsp_doc_border = true,
+          long_message_to_split = true
         },
         lsp = {
-          progress = { enabled = false }
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+          progress = { enabled = true }
+        },
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              kind = "",
+              find = "written",
+            },
+            opts = { skip = true },
+          },
         }
       })
+
+      require("notify").setup {
+        -- Complains if not set
+        background_colour = "#000000",
+        max_width = vim.o.columns / 4,
+        timeout = 800,
+        -- stages = "slide", -- Too slow
+        fps = 60
+      }
     end
   }
 }
