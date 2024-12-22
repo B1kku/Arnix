2f8b8797e1e6180519720fb95a4b1044b86c7ce5
Bikku <bikku+dev@slmail.me>
2024-12-23 00:17:51 +0100

    Update vers

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Personal NixOS config.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.12";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-24.12";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     firefox-addons = {
