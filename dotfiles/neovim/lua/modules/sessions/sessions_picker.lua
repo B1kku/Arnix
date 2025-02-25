763938e91d1daea32c4a81249bb45f5a1ab5b857
Bikku <bikku+dev@slmail.me>
2025-02-25 21:12:06 +0100

    Neovim: Rework session picker since dressing is archived and snacks is not a full replacement

--- dotfiles/neovim/lua/modules/sessions/sessions_picker.lua
+++ dotfiles/neovim/lua/modules/sessions/sessions_picker.lua
@@ -1,100 +1,140 @@
 local M = {}
-local telescope = require("telescope.themes").get_dropdown()
-local actions_state = require("telescope.actions.state")
-local directory = MiniSessions.config.directory
 
-local function format_name(name)
-  return name:sub(1, 1):upper() .. name:sub(2)
-end
+local minisessions = require("mini.sessions")
+local picker = nil
 
-local function get_detected_names()
-  local sessions_list = {}
-  local sessions = MiniSessions.detected
-  if sessions ~= nil then
-    local i = 0
-    for key, _ in pairs(sessions) do
-      i = i + 1
-      sessions_list[i] = key
-    end
+local opts = {
+  keys = {
+    actions = {
+      delete = { "i", "<C-d>" },
+      rename = { "i", "<C-r>" },
+      create = { "i", "<C-s>" },
+    },
+    custom = {}
+  }
+}
+
+function M.setup(user_opts)
+  if user_opts == nil then return end
+  if type(user_opts) == "function" then
+    user_opts = user_opts()
   end
-  return sessions_list
+  vim.tbl_deep_extend("force", user_opts, opts)
 end
 
-local function get_selected()
-  return actions_state.get_selected_entry()
-end
+local function get_actions()
+  local actions = require "telescope.actions"
+  local action_state = require "telescope.actions.state"
+  return {
+    select = function(prompt_bufnr)
+      actions.close(prompt_bufnr)
+      local selection = action_state.get_selected_entry()
+      minisessions.read(selection.value.name)
+    end,
+    -- https://github.com/nvim-telescope/telescope.nvim/issues/3384 Can't delete entries, so ig refresh the whole thing
+    delete = function(prompt_bufnr)
+      local selection = action_state.get_selected_entry()
+      local current_picker = action_state.get_current_picker(prompt_bufnr)
+      minisessions.delete(selection.value.name)
+      current_picker:refresh(current_picker.finder, { reset_prompt = false })
+    end,
+    rename = function(prompt_bufnr)
+      local selection = action_state.get_selected_entry()
+      local current_picker = action_state.get_current_picker(prompt_bufnr)
+      vim.ui.input({ prompt = "New session name:" }, function(input)
+        if not input or input == "" then
+          return
+        end
+        local session_file = selection.value.path
+        local path = vim.fn.fnamemodify(session_file, ":h")
+        path = vim.fs.joinpath(path, input)
 
-local function get_picker()
-  return actions_state.get_current_picker(vim.api.nvim_get_current_buf())
-end
+        local new_session = selection.value
+        new_session.name = input
+        new_session.path = path
+        minisessions.detected[input] = new_session
+        minisessions.detected[selection.value.name] = nil
+        os.rename(session_file, path)
 
-
-local function rename_session()
-  -- Missing session, AKA get current picker's selected value
-  local selected = get_selected()
-  local session = selected.value
-  vim.ui.input({ prompt = 'Rename ' .. session .. " to:" }, function(input)
-    if input then
-      input = format_name(input)
-      -- Is there no better way to do this? This breaks on windows...
-      os.rename(directory .. "/" .. session, directory .. "/" .. input)
-      -- MiniSessions.setup()
-      selected.value = input
-      selected.display = format_name(selected.value)
-      selected.ordinal = selected.display
-      get_picker():refresh()
+        current_picker:refresh(current_picker.finder, { reset_prompt = false })
+      end)
+    end,
+    create = function(prompt_bufnr)
+      local prompt = action_state.get_current_line()
+      local session = nil
+      if prompt ~= "" then
+        session = prompt
+      else
+        session = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+      end
+      minisessions.write(session)
+      local current_picker = action_state.get_current_picker(prompt_bufnr)
+      current_picker:refresh(current_picker.finder, { reset_prompt = true })
     end
-  end)
+  }
 end
 
-local function delete_session()
-  local picker = get_picker()
-  picker:delete_selection(function(selection)
-    MiniSessions.delete(selection.value)
-  end)
-end
+local function new_picker(telescope_opts)
+  local pickers = require "telescope.pickers"
+  local finders = require "telescope.finders"
+  local conf = require("telescope.config").values
+  local actions = require "telescope.actions"
 
-local function create_session()
-  local picker = get_picker()
-  local prompt = picker:_get_prompt()
-  local session = nil
-  if prompt ~= "" then
-    session = prompt
-  else
-    session = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-  end
-  local results = picker.finder.results
-  MiniSessions.write(format_name(session))
-  local entry = {}
-  entry.value = session
-  entry.display = format_name(session)
-  entry.ordinal = entry.display
-  entry.index = #results + 1
-  results[entry.index] = entry
-  picker:reset_prompt("")
-end
+  telescope_opts = telescope_opts or {}
+  local key_actions = get_actions()
+  local attach_mappings = function(_, map)
+    actions.select_default:replace(key_actions.select)
+    for key, value in pairs(opts.keys.actions) do
+      -- TODO: Proper message
+      if not key_actions[key] then
+        vim.print("Error: Session picker's keys.actions contained non-action key")
+        return false
+      end
+      map(value[1], value[2], key_actions[key], unpack(value, 3, #value))
+    end
 
+    for _, value in ipairs(opts.keys.custom) do
+      map(unpack(value))
+    end
 
-function M.open(keys)
-  telescope.attach_mappings = function(_, map)
-    map("i", keys.delete, delete_session)
-    map("i", keys.save, create_session)
-    map("i", keys.rename, rename_session)
     return true
   end
 
-  vim.ui.select(
-    get_detected_names(),
-    {
-      prompt = 'Select a Session',
-      telescope = telescope,
-      format_item = format_name
-    },
-    function(choice)
-      if choice then
-        MiniSessions.read(choice)
-      end
-    end)
+  local finder = finders.new_dynamic {
+    fn = function()
+      local sessions = vim.tbl_values(minisessions.detected)
+      table.sort(sessions, function(session, session_next)
+        return session.modify_time > session_next.modify_time
+      end)
+      return sessions
+    end,
+    entry_maker = function(entry)
+      return {
+        value = entry,
+        display = entry.name,
+        ordinal = entry.name
+      }
+    end
+  }
+  return pickers.new(telescope_opts, {
+    prompt_title = "Pick a session",
+    finder = finder,
+    sorter = conf.generic_sorter(telescope_opts),
+    attach_mappings = attach_mappings
+  })
 end
 
+M.open = function()
+  local ok, telescope_themes = pcall(require, "telescope.themes")
+  if not ok then
+    vim.print("Error: Telescope wasn't found, using the default picker")
+    M.open = minisessions.select
+    return
+  end
+  picker = new_picker(telescope_themes.get_dropdown {})
+  picker:find()
+  M.open = function()
+    picker:find()
+  end
+end
 return M
--- dotfiles/neovim/lua/plugins/sessions.lua
+++ dotfiles/neovim/lua/plugins/sessions.lua
@@ -1,6 +1,13 @@
+local session_select = function ()
+  require("mini.sessions").select()
+end
 return {
   'echasnovski/mini.sessions',
+  keys = {
+    { "<leader>st", session_select, "n", silent = true, desc = "Session picker" }
+  },
   config = function()
+    local minisessions = require("mini.sessions")
     local sessions_module = require("modules.sessions")
     require('mini.sessions').setup {
       autoread = false,
@@ -20,16 +27,10 @@ return {
         }
       }
     }
-    local keys = {
-      delete = "<C-d>",
-      save = "<C-s>",
-      rename = "<C-r>"
-    }
     -- Override minisessions' picker
-    MiniSessions.select = function()
-      require("modules.sessions.sessions_picker").open(keys)
-    end
-    -- TODO: Move to top
-    vim.keymap.set('n', '<leader>st', MiniSessions.select)
+    local custom_picker = require("modules.sessions.sessions_picker")
+    custom_picker.setup()
+    minisessions.select = custom_picker.open
+    session_select = minisessions.select
   end
 }
