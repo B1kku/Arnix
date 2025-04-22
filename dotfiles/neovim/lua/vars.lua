9a4ff00f6bbd661d0629d401620eeb6361aea9f1
Bikku <bikku+dev@slmail.me>
2025-04-22 14:10:33 +0200

    Misc, border updates from 0.11 (not yet working)

--- dotfiles/neovim/lua/vars.lua
+++ dotfiles/neovim/lua/vars.lua
@@ -1,14 +1,19 @@
 --[[ vars.lua ]]
 --
-local single = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
+-- local single = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
 local rounded = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
-local double = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" }
+-- local double = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" }
 local g = vim.g
 --  Theme  --
 g.t_co = 256
 g.background = "dark"
-
+-- NOTE: Should keep an eye on this, neovim finally has an standard
+-- vim.o.winborder
 g.borderchars = rounded
 g.border = "rounded"
 
 g.theme = "nightfox" -- Global variable for other plugins to use if needed.
+
+I = function(arg)
+  vim.print(vim.inspect(arg))
+end
