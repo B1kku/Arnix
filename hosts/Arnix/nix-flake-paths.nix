ef7e107b7c480353b44f3b8447e0c643049bd0df
Bikku <bikku+dev@slmail.me>
2025-08-29 21:10:22 +0200

    fix(nixos): fix and add proper config for nix registry paths.

--- hosts/Arnix/nix-flake-paths.nix
+++ hosts/Arnix/nix-flake-paths.nix
@@ -10,9 +10,12 @@
   nix = {
     settings = {
       # Path to nixpkgs, to follow flake inputs.
-      nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs-main";
+      nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
     };
-    registry.nixpkgs.flake = inputs.nixpkgs-main;
+    registry = {
+      nixpkgs.flake = inputs.nixpkgs-main;
+      nixpkgs-unstable.flake = inputs.nixpkgs-unstable;
+    };
     channel.enable = false;
   };
   environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs-main}";
